f1ac6217589cebcbacc5303af39968db8e6c564e
Adam Ahrens <adam.aahrens@gmail.com>
2016-12-15 14:29:05 -0600

    Adding bubble & selection sort

--- SwiftAlgorithms.playground/Pages/Sorts.xcplaygroundpage/Contents.swift
+++ SwiftAlgorithms.playground/Pages/Sorts.xcplaygroundpage/Contents.swift
@@ -6,7 +6,6 @@ extension Array where Element : Comparable {
 
   func insertionSort() -> Array<Element> {
     guard self.count > 1 else { return self }
-
     var copy = self
 
     for index in 1..<self.count {
@@ -24,7 +23,50 @@ extension Array where Element : Comparable {
 
     return copy
   }
+
+  func bubbleSort() -> Array<Element> {
+    guard self.count > 1 else { return self }
+    var copy = self
+
+    for index in 0..<copy.count {
+      // After each pass the last element will be in it's final position
+      let max = (copy.count - 1) - index
+      for innerIndex in 0..<max {
+        if copy[innerIndex] > copy[innerIndex + 1] {
+          swap(&copy[innerIndex], &copy[innerIndex + 1])
+        }
+      }
+    }
+
+    return copy
+  }
+
+  func selectionSort() -> Array<Element> {
+    guard self.count > 1 else { return self }
+    var copy = self
+
+    for index in 0..<copy.count {
+      var minIndex = index
+
+      // Find smallest element compared to current element if it exists
+      for innerIndex in (index+1)..<copy.count {
+        let compareElement = copy[innerIndex]
+        if compareElement < copy[minIndex] {
+          minIndex = innerIndex
+        }
+      }
+
+      // Found a smaller element. Swap
+      if minIndex != index {
+        swap(&copy[index], &copy[minIndex])
+      }
+    }
+
+    return copy
+  }
 }
 
 let unsorted = [5, 9, 2, 1, 6, 4, 3, 8, 7, 10]
-let sorted = unsorted.insertionSort()
+let sortedInsertion = unsorted.insertionSort()
+let sortedBubble = unsorted.bubbleSort()
+let sortedSelection = unsorted.selectionSort()
