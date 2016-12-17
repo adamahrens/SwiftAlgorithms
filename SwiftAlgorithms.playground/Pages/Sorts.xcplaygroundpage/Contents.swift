bbcf90751d7a37b00c2f4f5daa2875b01e80f2df
Adam Ahrens <adam.aahrens@gmail.com>
2016-12-16 21:31:50 -0600

    Adding quicksort

--- SwiftAlgorithms.playground/Pages/Sorts.xcplaygroundpage/Contents.swift
+++ SwiftAlgorithms.playground/Pages/Sorts.xcplaygroundpage/Contents.swift
@@ -64,9 +64,55 @@ extension Array where Element : Comparable {
 
     return copy
   }
+
+  mutating func quickSort() -> Array<Element> {
+    guard self.count > 1 else { return self }
+
+    func quickSortRecursive(start: Int, end: Int, array: inout Array<Element>) {
+      if start >= end { return }
+      let partition = pivot(start: start, end: end, array: &array)
+      quickSortRecursive(start: start, end: partition - 1, array: &array)
+      quickSortRecursive(start: partition + 1, end: end, array: &array)
+    }
+
+    func pivot(start: Int, end: Int, array: inout Array<Element>) -> Int {
+      let pivotValue = array[end]
+      var low = start
+      var high = end
+
+      while low < high {
+        while array[low] < pivotValue {
+          low += 1
+        }
+
+        while array[high] > pivotValue {
+          high -= 1
+        }
+
+        if low < high {
+          swap(&array[low], &array[high])
+        }
+      }
+
+      if low != end && array[low] < pivotValue {
+        swap(&array[low], &array[end])
+      }
+
+      return low
+    }
+
+    quickSortRecursive(start: 0, end: self.count - 1, array: &self)
+    return self
+  }
 }
 
-let unsorted = [5, 9, 2, 1, 6, 4, 3, 8, 7, 10]
+var unsorted = [5, 9, 2, 1, 6, 4, 3, 8, 7, 10]
+var reverse = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+var other = [10, 1, 9, 2, 8, 4, 7, 3, 6, 5]
+
 let sortedInsertion = unsorted.insertionSort()
 let sortedBubble = unsorted.bubbleSort()
 let sortedSelection = unsorted.selectionSort()
+let sortedQuicksort = unsorted.quickSort()
+let anotherQuicksort = reverse.quickSort()
+let againQuickstor = other.quickSort()
