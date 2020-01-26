6456fb4ecb1152f123cfd6b2ab47e3aa468fffe7
Adam Ahrens <adam.aahrens@gmail.com>
2020-01-26 14:11:18 -0600

    adding quicksort

--- AdvancedSorting.playground/Contents.swift
+++ AdvancedSorting.playground/Contents.swift
@@ -11,8 +11,8 @@ extension Array where Element: Comparable {
 
   private func qSort(array: inout Array<Element>, begin: Int, end: Int) -> Array<Element> {
     if begin < end {
-      print("Begin Index = \(begin), End Index = \(end). Array Slice is \(array[begin...end])")
       let partitionIndex = part(array: &array, begin: begin, end: end)
+      print("after partition \(array[begin...end])")
       qSort(array: &array, begin: begin, end: partitionIndex - 1)
       qSort(array: &array, begin: partitionIndex + 1, end: end)
     }
@@ -21,45 +21,49 @@ extension Array where Element: Comparable {
   }
 
   private func part(array: inout Array<Element>, begin: Int, end: Int) -> Int {
+
+    guard end - begin > 1 else {
+      if array[begin] > array[end] {
+        let temp = array[end]
+        array[end] = array[begin]
+        array[begin] = temp
+      }
+
+      return end
+    }
+
     let random = Int.random(in: begin...end)
     let pivot = array[random]
-    print("Pivot is \(pivot) at index \(random)")
     if random != begin {
       array.swapAt(random, begin)
-      print("Swapped pivot to begin \(array)")
     }
 
     var left = begin + 1
     var right = end
 
     while left < right {
-      print("left = \(left), right = \(right)")
       while array[left] < pivot {
-        print("moving left")
         left += 1
       }
 
       while array[right] > pivot {
-        print("moving right")
         right -= 1
       }
 
       if left < right {
-        // swap
-        let r = array[right]
+        let temp =  array[right]
         array[right] = array[left]
-        array[left] = r
-        print("Swapped \(array)")
+        array[left] = temp
       }
     }
 
+    // Did left and right cross
     if right < left {
-      let r = array[right]
-      array[begin] = r
-      array[right] = pivot
-      print("Swapped pivot at begin index to old right")
-      print("\(array)")
+      let temp =  array[right]
+      array[right] = array[begin]
+      array[begin] = temp
     }
+
     return right
   }
 }
